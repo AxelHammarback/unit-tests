c50ba595e0a0f7c17890f1a7f16a110a5cfe168e
AxelHammarback <axel.hammarback@gmail.com>
2020-10-11 23:03:33 +0200

    Unit tests done

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,8 @@
-export const hackerSpeak = (words) => {
+export const hackerSpeak = (words) => { 
   return words
-}
+  .replace(/a/gi, "4")
+  .replace(/i/gi, "1")
+  .replace(/s/gi, "5")
+  .replace(/o/gi, "0")
+  .replace(/e/gi, "3")
+};
